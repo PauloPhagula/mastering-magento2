30e7a6f9ad4d8dc92123af376324d152d369f579
Paulo Phagula <paulo.phagula@gmail.com>
2020-01-27 12:08:52 +0200

    Add controllers

---
+++ app/code/Mastering/SampleModule/Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Mastering\SampleModule\Controller\Adminhtml\Index;
+
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends \Magento\Backend\App\Action
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function execute()
+    {
+        /** @var \Magento\Framework\Controller\Result\Raw $result */
+        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+        $result->setContents("Hello Admins");
+
+        return $result;
+    }
+}
