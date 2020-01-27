67174e9c110557993402256b2b76fc8258214084
Paulo Phagula <paulo.phagula@gmail.com>
2020-01-27 12:08:35 +0200

    Add model layer

---
+++ app/code/Mastering/SampleModule/Model/ResourceModel/Item.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Mastering\SampleModule\Model\ResourceModel;
+
+
+use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+
+class Item extends AbstractDb
+{
+
+    /**
+     * @inheritDoc
+     */
+    protected function _construct()
+    {
+        $this->_init("mastering_sample_item", "id");
+    }
+}
