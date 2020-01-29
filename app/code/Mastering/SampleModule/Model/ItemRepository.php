e76eb8a7d84a6e1862baa5c8d3c9bfeb2d3584e1
Paulo Phagula <paulo.phagula@gmail.com>
2020-01-29 19:24:49 +0200

    Implement Web API

---
+++ app/code/Mastering/SampleModule/Model/ItemRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Mastering\SampleModule\Model;
+
+use Mastering\SampleModule\Api\ItemRepositoryInterface;
+use Mastering\SampleModule\Model\ResourceModel\Item\CollectionFactory;
+
+class ItemRepository implements ItemRepositoryInterface
+{
+    private $collectionFactory;
+
+    public function __construct(CollectionFactory $collectionFactory)
+    {
+        $this->collectionFactory = $collectionFactory;
+    }
+
+    public function getList()
+    {
+        $collection = $this->collectionFactory->create();
+        $items = $collection->getItems();
+        return $items;
+    }
+}
--- app/code/Mastering/SampleModule/Model/ResourceModel/Item/Collection.php
+++ app/code/Mastering/SampleModule/Model/ResourceModel/Item/Collection.php
@@ -3,7 +3,6 @@
 
 namespace Mastering\SampleModule\Model\ResourceModel\Item;
 
-
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 use Mastering\SampleModule\Model\Item;
 use Mastering\SampleModule\Model\ResourceModel\Item as ItemResource;
